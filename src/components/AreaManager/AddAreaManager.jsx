6519747c481b15ac3d8fecf68a8f3f5356185cfc
asumishra1698 <asumishra1698@gmail.com>
2025-04-26 21:55:11 +0530

    add area manager improved

--- src/components/AreaManager/AddAreaManager.jsx
+++ src/components/AreaManager/AddAreaManager.jsx
@@ -10,7 +10,7 @@ const AddAreaManager = () => {
     phone: "",
     address: "",
     idNumber: "",
-    profilePicture: null, // Added field for profile picture
+    profilePicture: null,
   });
 
   const handleChange = (e) => {
